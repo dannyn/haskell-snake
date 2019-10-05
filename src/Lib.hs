e1a4fef818815d462231b154b9d1ff455a0eb425
danny n <dnoest@gmail.com>
2019-10-04 20:23:50 -0400

    Direction should be a part of the snake.

--- src/Lib.hs
+++ src/Lib.hs
@@ -27,14 +27,15 @@ import System.Random
 
 type Coordinate  = (Int,Int)
 type Direction   = (Int,Int)
-type Snake       = [Coordinate]
 type Apple       = (Coordinate,StdGen)      
+
+data Snake = Snake { body :: [Coordinate]
+                   , sdir :: Direction } deriving (Eq, Show)
 
 data Status = Alive | Dead
     deriving (Show, Eq)
 
 data World = World { snake :: Snake 
-                   , dir :: Direction 
                    , time :: Float 
                    , status :: Status 
                    , apple :: Apple } deriving (Show)
@@ -56,9 +57,9 @@ dRight :: Direction
 dRight = (1,0)
 
 initialPosition :: Snake
-initialPosition = [(10,10), (10,9)]
+initialPosition = Snake [(10,10), (10,9)] dUp
 
-initialWorld = World initialPosition dUp 0 Alive ((10,1), mkStdGen 12345)
+initialWorld = World initialPosition 0 Alive ((10,1), mkStdGen 12345)
 
 snake_scale  :: Int
 snake_scale  = 10  
@@ -66,14 +67,14 @@ snake_scale  = 10
 inBox :: Coordinate -> Bool
 inBox (x,y) = (x >= 0) && (y >= 0) && (x < 64) && (y < 64)
 
-extend :: Snake -> Direction -> Snake
+extend :: [Coordinate] -> Direction -> [Coordinate]
 extend l@((x,y):xs) (dx,dy) = (x + dx, y + dy) : l
 
-move :: Snake -> Direction -> Snake
-move s d = init $ extend s d
+move :: Snake -> Snake
+move (Snake s d) = Snake (init $ extend s d) d
 
 changeDirection :: Direction ->  World -> World
-changeDirection new_d (World s d t st a) = World s new_d t st a
+changeDirection new_d (World (Snake s d) t st a) = World (Snake s new_d) t st a
 
 collides :: [Coordinate] -> Coordinate -> Bool                                                                                          
 collides l p =  (filter (\x -> x == p) l) /= []
@@ -85,18 +86,18 @@ nextApple (_, r) = ((a,b), r2)
     (b,r2) = randomR (0, height - 1) r1
 
 collidesApple :: Snake -> Apple -> Apple                                     
-collidesApple s a@(c, _) | (head s) == c = nextApple a
-                         | otherwise = a
+collidesApple (Snake s _) a@(c, _) | (head s) == c = nextApple a
+                                   | otherwise = a
 
 nextWorld :: World -> World
-nextWorld w@(World s d t st a@(cA,r)) | not $ inBox (head s) = (World initialPosition dUp 0.0 Dead (nextApple a))
-                              | collides (tail s) (head s) = (World initialPosition dUp 0.0 Dead (nextApple a))
-                              | cA == (head s) = (World (extend s d) d t st(nextApple a))
-                              | otherwise = w
+nextWorld w@(World s@(Snake b d) t st a@(cA,r)) | not $ inBox (head b) = (World initialPosition 0.0 Dead (nextApple a))
+                                                | collides (tail b) (head b) = (World initialPosition 0.0 Dead (nextApple a))
+                                                | cA == (head b) = (World ( Snake (extend b d) d) t st(nextApple a))
+                                                | otherwise = w
 
 step :: Float -> World -> World
-step dt (World s d t st a) | (dt + t) < speed = (World s d (dt + t) st a)
-                           | otherwise = (World (move s d) d 0.0 st a)
+step dt (World s t st a) | (dt + t) < speed = (World s (dt + t) st a)
+                         | otherwise = (World (move s) 0.0 st a)
 
 cartToScreen :: Coordinate -> Coordinate
 cartToScreen (x,y) = (x + width `div` 2, height `div` 2 - y)
@@ -114,12 +115,12 @@ snakePicture :: Snake -> Picture
 snakePicture s = pictures $ snakePicture' s
 
 snakePicture' :: Snake -> [Picture]
-snakePicture' [] = []
-snakePicture' ((x,y): xs) = sp : snakePicture' xs
+snakePicture' (Snake [] _) = []
+snakePicture' (Snake ((x,y):xs) d) = sp : snakePicture' (Snake xs d)
     where (xS, yS) = screenToCart (x,y)
           xC = fromIntegral (xS * 10) 
           yC = fromIntegral (yS * 10)
           sp = translate xC yC $ Color black $ rectangleSolid 10 10 
 
 worldPicture :: World -> Picture
-worldPicture (World s _ _ _ a) = pictures [snakePicture s, applePicture a]
+worldPicture (World s _ _ a) = pictures [snakePicture s, applePicture a]
--- test/Spec.hs
+++ test/Spec.hs
@@ -21,9 +21,8 @@ spec = do
         let d = (0,-1)
         (extend s d) `shouldBe` [(1,0), (1,1)]
     it "move" $ do
-        let s = [(1,1), (1,0)]
-        let d = (0,1)
-        (move s d) `shouldBe` [(1,2), (1,1)]
+        let s = Snake [(1,1), (1,0)] dDown
+        (move s) `shouldBe` (Snake [(1,2), (1,1)] dDown)
     it "snake self collision" $ do
         let s = [(1,1), (1,0), (0,0)]
         let c = (1,0)
@@ -33,10 +32,10 @@ spec = do
         let c = (5,5)
         (collides s c) `shouldBe` False
     it "apple collision" $ do
-        let s = [(1,1), (1,0), (0,0)]
+        let s = Snake [(1,1), (1,0), (0,0)] dDown
         let a = ((1,1), mkStdGen 12345)
         (fst $ collidesApple s a) `shouldNotBe` (fst a)
     it "apple non-collision" $ do
-        let s = [(1,1), (1,0), (0,0)]
+        let s = Snake [(1,1), (1,0), (0,0)] dDown
         let a = ((10,1), mkStdGen 12345)
         (fst $ collidesApple s a) `shouldBe` (fst a)
